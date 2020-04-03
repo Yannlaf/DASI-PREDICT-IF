25c5c8226fb4ca9261551a3a5184f53347bcd4b7
El Alaoui El Abdellaoui <ghali.elabdellaoui@gmail.com>
2020-04-03 19:43:22 +0300

    medium classes persistence

--- target/generated-sources/annotations/fr/insalyon/dasi/metier/modele/Cartomancien_.java
+++ target/generated-sources/annotations/fr/insalyon/dasi/metier/modele/Cartomancien_.java
@@ -3,7 +3,7 @@ package fr.insalyon.dasi.metier.modele;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-04-03T19:29:43")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-04-03T19:40:21")
 @StaticMetamodel(Cartomancien.class)
 public class Cartomancien_ extends Medium_ {
 
